6538e01829d3aea054842d1d3aab4d3ac2dbac6b
Forest Anderson <forestkzanderson@gmail.com>
2022-12-01 15:29:28 -0500

    Removing Django

--- django/tasks/admin.py
+++
@@ -1,8 +0,0 @@
-from django.apps import apps
-from django.contrib import admin
-
-models = apps.get_models()
-
-for model in models:
-    if "tasks.models" in str(model):
-        admin.site.register(model)
--- django/tasks/models.py
+++
@@ -1,5 +0,0 @@
-from django.db import models
-
-class Task(models.Model):
-    completed = models.BooleanField(default=False)
-    payload = models.JSONField(default=dict)
